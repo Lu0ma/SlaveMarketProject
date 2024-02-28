9596bee8034a32968bf21a817f5d0fc0ea29deb2
Josua <kaissgeek@gmail.com>
2024-02-28 16:59:49 +0100

    Update

--- HollowKnight/InteractableActor.cpp
+++ HollowKnight/InteractableActor.cpp
@@ -10,7 +10,6 @@ FloatRect InteractableActor::detector;
 InteractableActor::InteractableActor(const string& _name, const ShapeData& _data, const Vector2f& _sizeDetector) : Actor(_name , _data)
 {
 	sensorArea = new CircleShape(10.0f);
-	sensorArea->setPosition(this->GetShapePosition());
 	detector = sensorArea->getGlobalBounds();
 	canvas = new Canvas("Ui");
 	ShapeWidget* _widget = new ShapeWidget(ShapeData(this->GetShapePosition(), Vector2f(100.0f, 100.0f), PATH_HEALTH_MASK));
@@ -20,6 +19,7 @@ InteractableActor::InteractableActor(const string& _name, const ShapeData& _data
 void InteractableActor::Update(const float _deltaTime)
 {
 	Actor::Update(_deltaTime);
+	sensorArea->setPosition(this->GetShapePosition());
 	Player* _player = Game::GetPlayer();
 	if (detector.intersects(_player->GetDrawable()->getGlobalBounds()))
 	{
