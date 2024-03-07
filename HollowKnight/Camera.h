4e3425a53ff31e19c575ade689e41331acc68a11
Thomas <thomas-colin@hotmail.com>
2024-03-07 11:55:14 +0100

    new camera

--- HollowKnight/Camera.h
+++ HollowKnight/Camera.h
@@ -3,6 +3,16 @@
 
 using namespace sf;
 
+#define CAMERA_SHAKE_ANGLE 10.0f
+#define CAMERA_SHAKE_OFFSET 20.0f
+
+struct Shake
+{
+	Time current;
+	Time max;
+	float trauma;
+};
+
 class Camera
 {
 	float speed;
@@ -11,13 +21,15 @@ class Camera
 	Vector2f targetPosition;
 	Vector2f offset;
 	View view;
+	Shake shake;
+	Vector2f center;
 
 public:
 	View GetView() const
 	{
 		return view;
 	}
-	
+
 public:
 	Camera();
 
@@ -27,5 +39,9 @@ private:
 
 public:
 	void Init();
+	void Shake(const float _trauma, const float _duration);
 	void Update(const float _deltaTime);
+
+	//TODO move in macro
+	static inline float randn();
 };
