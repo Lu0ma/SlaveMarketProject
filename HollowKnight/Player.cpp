5be3d4d92ddcd3ef0709a4e054266c84fe3a188f
RaphBv <raphaelbouvier5@gmail.com>
2024-03-11 11:37:05 +0100

    DashFix

--- HollowKnight/Player.cpp
+++ HollowKnight/Player.cpp
@@ -92,7 +92,7 @@ void Player::SetupPlayerInput()
 			}, InputData({ ActionType::JoystickButtonPressed, Joystick::isButtonPressed(0,1) })),
 
 		ActionData("Dash", [&]() { movement->Dash(); FxManager::GetInstance().Run("FxDash"); }, InputData({ActionType::KeyPressed,Keyboard::LControl})),
-		ActionData("StopDash", [&]() { movement->SetDirectionX(movement->GetDashDirection(), "Right"); }, InputData({ActionType::KeyReleased,Keyboard::LControl})),
+		ActionData("StopDash", [&]() { movement->SetDirectionX(0, "StopRight"); }, InputData({ActionType::KeyReleased,Keyboard::LControl})),
 		ActionData("ControllerDash", [&]() {
 
 			if (Joystick::isButtonPressed(0, 7)) {
